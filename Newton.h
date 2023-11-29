3f94be3c59a30e583f1d36b80b437b55969e466e
MiraslauK <151074739+MiraslauK@users.noreply.github.com>
2023-11-30 02:15:42 +0300

    1

---
+++ Newton.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+double** initial(int n, int m);
+
+double function1(double x1, double x2);
+
+double function2(double x1, double x2);
+
+double func11(double x1, double x2);
+
+double func12(double x1, double x2);
+
+double func21(double x1, double x2);
+
+double func22(double x1, double x2);
+
+void J(double** a, double x1, double x2);
+
+void minus_vector_nevjazki(double* F, double x1, double x2);
+
+double* gauss(double** matrix, int n, int m);
+
+double* newton(int n, double x1, double x2);
+
+void printResult(double* res, int n);
